133750a96490200e4ab466b8e1af1ee95698a232
andrey <andreilkt@yandex.ru>
2022-12-21 12:33:52 +0700

    Созданы и зарегистрированы  модели, выполнена миграция

--- fly/fly_nsk/admin.py
+++ fly/fly_nsk/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.db import models
+from fly_nsk.models import Betasevis, BetasevisText
 
-# Register your models here.
+#from fly.fly_nsk.models import BetasevisText BetasevisText
+
+@admin.register(Betasevis)
+class BetasevisAdmin(admin.ModelAdmin):
+    list_display = ("main", "services", "works", "forms", "contacts", "site", "outsorcing")
+
+@admin.register(BetasevisText)
+class BetaservisTextAdmin(admin.ModelAdmin):
+    list_display = ("main_text", "services_text", "works_text", "forms_text", "contacts_text", "site_text", "outsorcing_text")
+
--- fly/fly_nsk/models.py
+++ fly/fly_nsk/models.py
@@ -4,20 +4,35 @@ from django.utils import timezone
 
 
 class Betasevis(models.Model):
-    main = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    main = models.TextField()
     services = models.TextField()
     works = models.TextField()
     forms = models.TextField()
     contacts = models.TextField()
-    forms = models.TextField()
     site = models.TextField()
     outsorcing = models.TextField()
     #created_date = models.DateTimeField(default=timezone.now)
     #published_date = models.DateTimeField(blank=True, null=True)
 
+def __str__(self):
+    return f"{self.main}, {self.services}, {self.works}, {self.forms}, {self.contacts}, {self.site}, {self.outsorcing}"
+
+class BetasevisText(models.Model):
+    main_text = models.TextField()
+    services_text = models.TextField()
+    works_text = models.TextField()
+    forms_text = models.TextField()
+    contacts_text = models.TextField()
+    site_text = models.TextField()
+    outsorcing_text = models.TextField()
+    Betasevis = models.OneToOneField(Betasevis, on_delete=models.CASCADE, primary_key=True)
+def __str__(self):
+    return f"{self.main_text}, {self.services_text}, {self.works_text}, {self.forms_text}, {self.contacts_text}, {self.site_text}, {self.outsorcing_text}"
+
+"""
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+"""
 
-    def __str__(self):
-        return self.title
+
